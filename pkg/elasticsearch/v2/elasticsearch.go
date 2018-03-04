0dd30a956bb34a2a4f3bf26b67684cac768341bf
rafael_chicoli <rafael_chicoli@regis24.de>
2018-03-04 12:17:32 +0100

    driver: add elasticsearch-insecure option

--- pkg/elasticsearch/v2/elasticsearch.go
+++ pkg/elasticsearch/v2/elasticsearch.go
@@ -17,14 +17,14 @@ type Elasticsearch struct {
 	indexService *elastic.IndexService
 }
 
-func NewClient(address, username, password string, timeout int, sniff bool) (elasticsearch.Client, error) {
+func NewClient(address, username, password string, timeout int, sniff bool, insecure bool) (elasticsearch.Client, error) {
 
 	url, _ := url.Parse(address)
 	tr := new(http.Transport)
 
 	if url.Scheme == "https" {
 		tr = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
 		}
 	}
 	client := &http.Client{Transport: tr}
