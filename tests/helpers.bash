b0422d6d345d639caea317c13a4ad78dba50ca18
rafael_chicoli <rafael_chicoli@regis24.de>
2018-03-07 18:57:20 +0100

    tests: add tls variable to get protocol function

--- tests/helpers.bash
+++ tests/helpers.bash
@@ -21,10 +21,12 @@ DOCKER_COMPOSE_FILE="${DOCKER_COMPOSE_DIR}/docker-compose.yml"
 ELASTICSEARCH_HTTP_URL="http://${ELASTICSEARCH_IP}:${ELASTICSEARCH_PORT}"
 ELASTICSEARCH_HTTPS_URL="https://${ELASTICSEARCH_IP}:${ELASTICSEARCH_PORT}"
 
-ELASTICSEARCH_URL="$ELASTICSEARCH_HTTP_URL"
-if [[ "$TLS" == "true" ]]; then
-  ELASTICSEARCH_URL="$ELASTICSEARCH_HTTPS_URL"
-fi
+function _getProtocol(){
+  ELASTICSEARCH_URL="$ELASTICSEARCH_HTTP_URL"
+  if [[ "$TLS" == "true" ]]; then
+    ELASTICSEARCH_URL="$ELASTICSEARCH_HTTPS_URL"
+  fi
+}
 
 ELASTICSEARCH_USERNAME="${ELASTICSEARCH_USERNAME:-elastic}"
 ELASTICSEARCH_PASSWORD="${ELASTICSEARCH_PASSWORD:-changeme}"
@@ -33,17 +35,19 @@ MAKEFILE="${BASE_DIR}/Makefile"
 
 function _post() {
   local id="$1"
-  curl -XPOST -H "Content-Type: application/json" -d "{\"message\":\"$1\"}" "http://${WEBAPPER_IP}:${WEBAPPER_PORT}/log" &>/dev/null
+  curl -s -XPOST -H "Content-Type: application/json" --data "{\"message\":\"$1\"}" "http://${WEBAPPER_IP}:${WEBAPPER_PORT}/log" &>/tmp/test.log
   # wait 5 seconds until the message can be processed, just in case if there is a system load
   sleep 5
 }
 
 function _search() {
+    _getProtocol
     local message="$1"
     curl -G -s -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" ${ELASTICSEARCH_URL}/${ELASTICSEARCH_INDEX}/${ELASTICSEARCH_TYPE}/_search\?pretty=true\&size=1 --data-urlencode "q=message:\"${message}\""
 }
 
 function _fields() {
+    _getProtocol
     local message="$1"
     curl -G -s -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" ${ELASTICSEARCH_URL}/${ELASTICSEARCH_INDEX}/${ELASTICSEARCH_TYPE}/_search\?pretty=true\&size=1 --data-urlencode "q=message:\"${message}\"" | jq '.hits.hits[0]._source' | jq -r 'keys[]'
 }
