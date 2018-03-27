754b9e714d47771f7131d1d23ad97a6a66803d84
rafael_chicoli <rafael_chicoli@regis24.de>
2018-03-27 21:00:19 +0200

    ci: add major and feature env to the release script

--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -9,7 +9,19 @@ git fetch --tags
 LAST_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)" 2>/dev/null)"
 
 # create a new tag version
-NEW_TAG="$(awk -F. '{printf "%d.%d.%d", $1, $2, $3+1}' <(echo "$LAST_TAG"))"
+if [[ -n "${RELEASE_TAG:+x}" ]]; then
+    NEW_TAG="$RELEASE_TAG"
+elif [[ -n "${MAJOR_RELEASE:+x}" ]]; then
+    NEW_TAG="$(awk -F. '{printf "%d.0.0", $1+1 }' <(echo "$LAST_TAG"))"
+elif [[ -n "${FEATURE_RELEASE:+x}" ]]; then
+    NEW_TAG="$(awk -F. '{printf "%d.%d.0", $1, $2+1 }' <(echo "$LAST_TAG"))"
+else
+    NEW_TAG="$(awk -F. '{printf "%d.%d.%d", $1, $2, $3+1}' <(echo "$LAST_TAG"))"
+fi
+
+if [[ "$NEW_TAG" == "$LAST_TAG" ]]; then
+    NEW_TAG="$(awk -F. '{printf "%d.%d.%d", $1, $2, $3+1}' <(echo "$NEW_TAG"))"
+fi
 
 export PLUGIN_TAG="$NEW_TAG"
 make
@@ -19,6 +31,8 @@ make push
 
 # git log --oneline "${LAST_TAG}..HEAD"
 
+# FIXME: travis deploy extension cuts multiple commit lines
+# possible solution: remove deploy extension and add git push tag command
 git tag -a "$NEW_TAG" -m "$(git log --oneline ${LAST_TAG}..HEAD)"
 
 git tag -n100
