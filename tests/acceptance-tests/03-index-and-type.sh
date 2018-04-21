db945520fc5358d7a7c22ff9e6d9df7428d64224
rafael_chicoli <rafael_chicoli@regis24.de>
2018-04-21 14:09:45 +0200

    tests: fix test for indexName regex

--- tests/acceptance-tests/03-index-and-type.sh
+++ tests/acceptance-tests/03-index-and-type.sh
@@ -48,12 +48,12 @@ function test_different_index_and_type(){
 
 function test_index_regex_year_month_day(){
 
-  description="[${BASHT_TEST_FILENAME##*/}] acceptance-tests (v${CLIENT_VERSION}): $BASHT_TEST_NUMBER - index regex %Y.%m.%d are converted to the current date"
+  description="[${BASHT_TEST_FILENAME##*/}] acceptance-tests (v${CLIENT_VERSION}): $BASHT_TEST_NUMBER - index regex %F are converted to the current date"
   echo "$description"
 
   basht_run make -f "$MAKEFILE" deploy_elasticsearch
 
-  export ELASTICSEARCH_INDEX="docker-%Y.%m.%d"
+  export ELASTICSEARCH_INDEX="docker-%F"
 
   name="${BASHT_TEST_FILENAME##*/}.${BASHT_TEST_NUMBER}"
   message="$((RANDOM)) $description"
@@ -80,12 +80,12 @@ function test_index_regex_year_month_day(){
 
 function test_index_regex_year_month_day(){
 
-  description="[${BASHT_TEST_FILENAME##*/}] acceptance-tests (v${CLIENT_VERSION}): $BASHT_TEST_NUMBER - index regex %F are converted to the current date"
+  description="[${BASHT_TEST_FILENAME##*/}] acceptance-tests (v${CLIENT_VERSION}): $BASHT_TEST_NUMBER - all index regex together are converted to the current date"
   echo "$description"
 
   basht_run make -f "$MAKEFILE" deploy_elasticsearch
 
-  export ELASTICSEARCH_INDEX="docker-%F"
+  export ELASTICSEARCH_INDEX="test-%b-%B-%d-%j-%m-%y-%y"
 
   name="${BASHT_TEST_FILENAME##*/}.${BASHT_TEST_NUMBER}"
   message="$((RANDOM)) $description"
@@ -100,10 +100,10 @@ function test_index_regex_year_month_day(){
 
   sleep "${SLEEP_TIME}"
 
-  index_name="$(echo "docker-$(date +%F)")"
+  index_name="$(echo "test-$(date +%b-%B-%d-%j-%m-%y-%y)")"
 
   basht_run curl -s -G --connect-timeout 5 \
-    "${ELASTICSEARCH_URL}/${index_name}/_search?pretty=true&size=1" \
+    "${ELASTICSEARCH_URL}/${index_name,,}/_search?pretty=true&size=1" \
     --data-urlencode "q=message:\"$message\""
 
   basht_assert "echo '${output}' | jq -r '.hits.hits[0]._source.message'" equals "$message"
